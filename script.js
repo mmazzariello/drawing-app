f32294aa1f26b7b95d725be73800350a173d0895
Mariana Mazzariello <mfmazzariello@gmail.com>
2022-01-06 11:49:18 +0100

    UPDATE addEventListener, when mouse move, I called drawCircle and drawLine functions

--- script.js
+++ script.js
@@ -2,9 +2,51 @@ var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
 
 let size = 20;
+let isPressed = false;
 let color = "black";
 let x;
 let y;
+
+//EVENT LISTENERS
+
+//MOUSEDOWN: To get x, y values:
+
+canvas.addEventListener("mousedown", (e) => {
+  isPressed = true;
+  x = e.offsetX;
+  y = e.offsetY;
+
+  console.log(isPressed, x, y);
+});
+
+//MOUSEUP: When release, I need to clear these values:
+
+canvas.addEventListener("mouseup", (e) => {
+  isPressed = false;
+  x = undefined;
+  y = undefined;
+
+  console.log("release", isPressed, x, y);
+});
+
+//MOUSEMOVE: When mouse move:
+
+canvas.addEventListener("mousemove", (e) => {
+  if (isPressed) {
+    const x2 = e.offsetX;
+    const y2 = e.offsetY;
+
+    console.log("move", x2, y2);
+
+    drawCircle(x2, y2);
+    drawLine(x, y, x2, y2);
+
+    x = x2;
+    y = y2;
+  }
+});
+
+//FUNCTIONS
 
 function drawCircle(x, y) {
   ctx.beginPath();
@@ -18,7 +60,7 @@ function drawLine(x1, y1, x2, y2) {
   ctx.moveTo(x1, y1);
   ctx.lineTo(x2, y2);
   ctx.strokeStyle = color;
-  ctx.lineWidth = size;
+  ctx.lineWidth = size * 2;
   ctx.stroke();
 }
 
