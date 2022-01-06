76fa798515eaecbc1ac5141bcc44a853c0dd05d8
Mariana Mazzariello <mfmazzariello@gmail.com>
2022-01-06 11:33:39 +0100

    ADD a couple functions: drawCircle & drawLine with mdn web docs

---
+++ script.js
@@ -0,0 +1,26 @@
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+
+let size = 20;
+let color = "black";
+let x;
+let y;
+
+function drawCircle(x, y) {
+  ctx.beginPath();
+  ctx.arc(x, y, size, 0, Math.PI * 2, true);
+  ctx.fillStyle = color;
+  ctx.fill();
+}
+
+function drawLine(x1, y1, x2, y2) {
+  ctx.beginPath();
+  ctx.moveTo(x1, y1);
+  ctx.lineTo(x2, y2);
+  ctx.strokeStyle = color;
+  ctx.lineWidth = size;
+  ctx.stroke();
+}
+
+// drawCircle(100, 200);
+// drawLine(300, 300, 200, 400);
